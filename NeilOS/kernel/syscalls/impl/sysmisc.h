cd8e097c7bb4020a1af3ce6f0fd04cc649da70f7
Neil Singh <Neil@neils-macbook-pro.local>
2017-07-08 12:54:04 -0700

    Forgot two files.

---
+++ NeilOS/kernel/syscalls/impl/sysmisc.h
@@ -0,0 +1,20 @@
+//
+//  sysmisc.h
+//  NeilOS
+//
+//  Created by Neil Singh on 6/29/17.
+//  Copyright © 2017 Neil Singh. All rights reserved.
+//
+
+#ifndef SYSMISC_H
+#define SYSMISC_H
+
+#include <common/types.h>
+
+// Get some info about a limit
+uint32_t sysconf(int name);
+
+// Get most recent error number
+uint32_t sys_errno();
+
+#endif /* SYSMISC_H */
